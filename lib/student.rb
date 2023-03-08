194b0a1e58b371d5619822c6945534f00a9395e1
Victor <victorgmassimi.microverse@gmail.com>
2023-03-08 08:38:28 -0600

    classroom-student association

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,7 @@
 require_relative 'person'
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
@@ -7,5 +9,10 @@ class Student < Person
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 end
