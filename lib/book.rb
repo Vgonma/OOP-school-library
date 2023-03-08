95caad91161d6f923cf3fbf01b2d9c1cf505caef
Victor <victorgmassimi.microverse@gmail.com>
2023-03-08 09:24:34 -0600

    Add relationship for book-renta & person-rental

---
+++ lib/book.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.book = self
+    rental.person.rentals.push(rental) unless rental.person.rentals.include?(rental)
+  end
+end
